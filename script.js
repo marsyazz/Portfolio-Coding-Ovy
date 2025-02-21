250737fdf64470f30d1755f532377dd0a57326dc
marsyazz <marsyazieera@gmail.com>
2025-02-21 10:39:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,12 @@ window.addEventListener("scroll", function() {
         header.classList.remove("scrolled");
     }
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.querySelector("nav ul");
+
+    menuToggle.addEventListener("click", function() {
+        this.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+});
